89421348e87fdfbe905f1796e57c278082c5970d
yeptony <thomasgrosjean00@gmail.com>
2017-01-12 21:37:59 +0100

    add functionalities

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.end('Hi!')
+  var ip = req.ip
+  var language = req.headers['accept-language']
+  var software = req.headers['user-agent']
+  var json = {
+    "ip": ip.slice(7),
+    "language": language.split(',')[0],
+    "software": software.split('(')[1].split(')')[0]
+  }
+
+  res.end(JSON.stringify(json))
 })
 
 app.listen(process.env.PORT || 3000, function () {
